9b04fade0bdee331089f929470d831b51ec09583
Ana Franco <115043626+anamspe@users.noreply.github.com>
2025-06-26 16:47:48 -0700

    Update resume link

--- src/constants.js
+++ src/constants.js
@@ -5,12 +5,10 @@ export const dialogueData = {
 I'm currently learning Go and Typescript. But regardless of the language, I just like programming.
     Here is my <a href="https://github.com/anamspe" target="_blank">Github</a>!`,
   "cs-degree": `This is my Web Development diploma. I hung it on the wall because I'm proud of it. It was important for me to go through that process so I could have some guidance and foundation in my journey as a web developer.`,
-  "sofa-table": `That's my sofa. I like to relax here and play videogames. 
-  I am also a gaming content creator. Go check <a href="https://www.twitch.tv/anamspe" target="_blank">my channel</a>! (Give a follow if you like the content!)
-  I mostly try to have fun while playing and talking to chat. :)`,
-  tv: `That's my TV. I've been watching a lot of anime recently. My favourites so far are: Jujutsu Kaisen, Frieren: Beyond Journey's End, My Dress Up Darling and Skip and Loafer. I'm also a big Law & Order: SVU's fan!`,
+  "sofa-table": `That's my sofa. I like to relax here, watch anime, TV series, movies and play videogames.`,
+  tv: `That's my TV. I've been watching a lot of anime recently. My favourites so far are: Jujutsu Kaisen, Frieren: Beyond Journey's End, and My Dress Up Darling I'm also a big Law & Order: SVU's fan!`,
   bed: `This where I sleep. I find that when I'm stuck in a problem, if I sleep on it, usually I find the answer to it right after!`,
-  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1BdlIlqZlU1f06wHERTQcl9yGae9HeEbK/view" target="_blank">Check it out?</a>
+  resume: `This is my desk and on it is my resume. <a href="https://flowcv.com/resume/abqo56qesgp1" target="_blank">Check it out?</a>
   Contact me at anamspe.fr@gmail.com if you have any interesting job opportunities!`,
   projects: `This portfolio was created with the Kaboom.js library, which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. It was only possible by JSLegendDev's tutorial, which you can find <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
